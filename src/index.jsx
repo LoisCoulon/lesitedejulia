9fce251c43a450659f936dea8f52918dac926a5e
gimefaya <flojuve@hotmail.fr>
2024-02-26 17:14:11 +0100

    update for gh pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home/Home';
-import About from './Pages/About/About'
-import Header from './Components/Header/Header';
-import Error from './Pages/Error/Error'
-import Contact from './Pages/Contact/Contact';
-import Footer from './Components/Footer/Footer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Header from "./Components/Header/Header";
+import Error from "./Pages/Error/Error";
+import Contact from "./Pages/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import "./main.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Header/>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='*' element={<Error/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </Router>
   </React.StrictMode>
 );
-
